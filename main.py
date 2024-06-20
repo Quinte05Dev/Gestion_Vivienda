5fa21bcdab42bc6de27b5a31980f57a20789c651
Quinte05Dev <Quinte05Dev@gmail.com>
2024-06-19 21:03:36 -0500

    Se comenta main.py linea por linea

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# Importa la clase FastAPI del módulo fastapi. Esta clase es usada para crear una instancia de la aplicación web.
 from fastapi import FastAPI
+# Importa el módulo proyectos desde la carpeta routers donde estan los endpoints.
 from routers import proyectos
 
+# Creamos una instancia de la aplicación FastAPI. Esta instancia es la que maneja todas las peticiones entrantes.
 app = FastAPI()
 
+# Esto nos permite que las rutas definidas en el archivo proyectos.py sean accesibles a través de la aplicación.
 app.include_router(proyectos.router)
 
 
